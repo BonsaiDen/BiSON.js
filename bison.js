bfc25c895916937711691919db10a6edf294b64b
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-10-29 20:02:48 +0200

    Do some cleanup.

--- bison.js
+++ bison.js
@@ -132,16 +132,16 @@ function encode(data) {
 
 function decode(data) {
     var p = 0, l = data.length;
-    var s = [], d = undefined, f = null, t = 0, i = -1;
+    var stack = [], dec = undefined, f = null, t = 0, i = -1;
     var dict = false, set = false;
-    var k = '', e = null, r = 0;
+    var key = '', e = null, r = 0;
     while (p < l) {
         t = data.charCodeAt(p++);
-        f = s[i];
+        f = stack[i];
         
         // Keys
         if (dict && set && t > 16) {
-            k = data.substring(p, p + t - 17);
+            key = data.substring(p, p + t - 17);
             p += t - 17;
             set = false;
         
@@ -149,23 +149,22 @@ function decode(data) {
         } else if (t === 8 || t === 10) {
             e = t === 8 ? new Array() : new Object();
             set = dict = t === 10;
-            d !== undefined ? f instanceof Array ? f.push(e) : f[k] = e : d = e;
-            s.push(e);
+            dec !== undefined ? f instanceof Array ? f.push(e) : f[key] = e : dec = e;
+            stack.push(e);
             i++;
         
         } else if (t === 11 || t === 9) {
-            s.pop();
-            set = dict = !(s[--i] instanceof Array);
+            stack.pop();
+            set = dict = !(stack[--i] instanceof Array);
         
         // Fixed
         } else if (t > 16) {
             t = t - 17;
             t = t > 115 ? (0 - t + 116) : t + 1;
-            f instanceof Array ? f.push(t) : f[k] = t;
+            f instanceof Array ? f.push(t) : f[key] = t;
             set = true;
         
         } else if (t > 0 && t < 5) {
-            e = 0;
             if (((t - 1) / 2 | 0) === 0) {
                 e = data.charCodeAt(p);
                 p++;
@@ -175,7 +174,7 @@ function decode(data) {
                 p += 2;
             }
             e = t % 2 ? e + 1 : 0 - e;
-            f instanceof Array ? f.push(e) : f[k] = e;
+            f instanceof Array ? f.push(e) : f[key] = e;
             set = true;
         
         // Floats
@@ -199,33 +198,33 @@ function decode(data) {
             }
             
             e = t % 2 ? e + r * 0.01 : 0 - (e + r * 0.01);
-            f instanceof Array ? f.push(e) : f[k] = e;
+            f instanceof Array ? f.push(e) : f[key] = e;
             set = true;
         
         // Booleans
         } else if (t > 4 && t < 7) {
-            f instanceof Array ? f.push(t === 5) : f[k] = t === 5;
+            f instanceof Array ? f.push(t === 5) : f[key] = t === 5;
             set = true;
         
         // Null
         } else if (t === 0) {
-            f instanceof Array ? f.push(null) : f[k] = null;
+            f instanceof Array ? f.push(null) : f[key] = null;
             set = true;
         
         // Strings
         } else if (t === 7) {
-            r = 0;
+            e = 0;
             while (data.charCodeAt(p) === 65535) {
-                r += 65535;
+                e += 65535;
                 p++;
             }
-            r += data.charCodeAt(p++);
-            f instanceof Array ? f.push(data.substr(p, r)) : f[k] = data.substr(p, r);
-            p += r;
+            e += data.charCodeAt(p++);
+            f instanceof Array ? f.push(data.substr(p, e)) : f[key] = data.substr(p, e);
+            p += e;
             set = true;
         }
     }
-    return d;
+    return dec;
 }
 
 if (typeof window === 'undefined') {
